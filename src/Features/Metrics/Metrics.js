9ff25698700008f52f8ecb6af4cb8694cfec790b
jschles-sooryen <john.schlesinger@sooryen.com>
2021-08-16 14:30:50 -0400

    Add recharts, add MetricsSelect, include MetricsSelect in Metrics component

--- src/Features/Metrics/Metrics.js
+++ src/Features/Metrics/Metrics.js
@@ -1,3 +1,28 @@
 import React from 'react';
+import { useQuery, useSubscription } from '@apollo/client';
+import { LinearProgress, Typography, Container } from '@material-ui/core';
 
-export default () => <div>Metrics</div>;
+import MetricsSelect from './MetricsSelect';
+import { getMetricsQuery } from '../../graphql/queries';
+import { newMeasurementSubscription } from '../../graphql/subscriptions';
+
+const Metrics = () => {
+  const { loading, error, data } = useQuery(getMetricsQuery);
+  const {
+    data: subData, error: subError, loading: subLoading,
+  } = useSubscription(newMeasurementSubscription);
+
+  if (loading || subLoading) return <LinearProgress />;
+  if (error) return <Typography color="error">{error}</Typography>;
+  if (subError) return <Typography color="error">{subError}</Typography>;
+
+  const metricsOptions = data?.getMetrics;
+
+  return (
+    <Container>
+      <MetricsSelect options={metricsOptions} />
+    </Container>
+  );
+};
+
+export default Metrics;
---
+++ src/Features/Metrics/MetricsSelect.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { arrayOf, string } from 'prop-types';
+import {
+  Select, MenuItem, InputLabel, Chip, FormControl, makeStyles,
+} from '@material-ui/core';
+import { Cancel } from '@material-ui/icons';
+import { addSelectedMetric, removeSelectedMetric } from '../../store/actions';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: 755,
+  },
+}));
+
+const MetricsSelect = ({ options }) => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const { selectedMetrics } = useSelector((state) => state.metrics);
+  const [isSelectOpen, setIsSelectOpen] = useState(false);
+
+  const metricOptions = options.filter((option) => !selectedMetrics.includes(option));
+
+  useEffect(() => {
+    if (!metricOptions.length && isSelectOpen) {
+      setIsSelectOpen(false);
+    }
+  }, [selectedMetrics, metricOptions, isSelectOpen]);
+
+  const handleChange = (e) => {
+    // last string in e.target.value array will be the added metric
+    const addedMetric = e.target.value[e.target.value.length - 1];
+
+    if (addedMetric && !selectedMetrics.includes(addedMetric)) {
+      dispatch(addSelectedMetric(addedMetric));
+    }
+  };
+
+  const handleOnDelete = (_, value) => {
+    dispatch(removeSelectedMetric(value));
+  };
+
+  const handleOpen = () => {
+    setIsSelectOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsSelectOpen(false);
+  };
+
+  return (
+    <FormControl classes={{ root: classes.root }}>
+      <InputLabel id="metrics-select">Select Metric(s)</InputLabel>
+      <Select
+        labelId="metrics-select"
+        multiple
+        open={isSelectOpen}
+        onOpen={handleOpen}
+        onClose={handleClose}
+        value={selectedMetrics}
+        onChange={handleChange}
+        disabled={!metricOptions.length}
+        renderValue={(selected) => (
+          <div>
+            {selected.map((value) => (
+              <Chip
+                key={value}
+                label={value}
+                deleteIcon={<Cancel onMouseDown={(e) => e.stopPropagation()} />}
+                onDelete={(e) => handleOnDelete(e, value)}
+              />
+            ))}
+          </div>
+        )}
+        MenuProps={{
+          anchorOrigin: {
+            vertical: 'bottom',
+            horizontal: 'left',
+          },
+          transformOrigin: {
+            vertical: 'top',
+            horizontal: 'left',
+          },
+          getContentAnchorEl: null,
+        }}
+      >
+        {metricOptions.map((option) => (
+          <MenuItem value={option} key={option}>{option}</MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+MetricsSelect.propTypes = {
+  options: arrayOf(string),
+};
+
+export default MetricsSelect;
