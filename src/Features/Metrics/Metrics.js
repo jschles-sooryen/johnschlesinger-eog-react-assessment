874eec8cbb44611000d69fa0936bb5eba696a29e
jschles-sooryen <john.schlesinger@sooryen.com>
2021-08-17 17:31:54 -0400

    Use LoadingIndicator instead of LinearProgress for loading state on Metrics

--- src/Features/Metrics/Metrics.js
+++ src/Features/Metrics/Metrics.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { LinearProgress, Typography, Container } from '@material-ui/core';
+import { Typography, Container } from '@material-ui/core';
 
 import MetricsSelect from './MetricsSelect';
 import MetricRealTimeInfo from './MetricRealTimeInfo';
 import MetricsLineChart from './MetricsLineChart';
 import { getMetricsQuery } from '../../graphql/queries';
+import LoadingIndicator from '../../components/LoadingIndicator';
 
 const Metrics = () => {
   const { loading, error, data } = useQuery(getMetricsQuery);
 
-  if (loading) return <LinearProgress />;
+  if (loading) return <LoadingIndicator />;
   if (error) return <Typography color="error">{error}</Typography>;
 
   const metricsOptions = data?.getMetrics;
